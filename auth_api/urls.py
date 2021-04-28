3d5047f74ce77d89e201f58dd111d540d45b2ab1
Jules LE BRIS <jls.lebris@gmail.com>
2021-04-28 21:22:22 +0200

    ✨ add refresh token api point and incr token life

--- auth_api/urls.py
+++ auth_api/urls.py
@@ -2,11 +2,13 @@ from django.contrib.auth.models import User
 from django.urls import include, path, re_path
 from django.urls.conf import include, path
 from rest_framework import routers, serializers, viewsets
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from . import views
 
 urlpatterns = [
     re_path(r"^totp/create/$", views.TOTPCreateView.as_view(), name="totp-create"),
     re_path(r"^totp/login/(?P<token>[0-9]{6})/$", views.TOTPVerifyView.as_view(), name="totp-login"),
-    path("get-token/", views.CustomAuthToken.as_view()),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/login/", views.CustomAuthToken.as_view()),
 ]
